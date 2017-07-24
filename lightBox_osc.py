acf6fde6d9a8008f23116b9c5a3526a1b9193feb
Callum Kirkwood <CallumKirkwood@Toms-MacBook-Air.local>
2017-07-24 21:14:11 +0100

    add lightBox controller

---
+++ lightBox_osc.py
@@ -0,0 +1,63 @@
+from OSC import OSCServer
+from time import sleep
+from threading import Thread
+from dotenv import load_dotenv, find_dotenv
+from neopixel import *
+import signal
+import sys
+import os
+
+# LED strip configuration:
+LED_COUNT      = 48
+LED_PIN        = 18
+LED_FREQ_HZ    = 800000
+LED_DMA        = 5
+LED_BRIGHTNESS = 255
+LED_INVERT     = False
+
+# Create NeoPixel object, initialise library
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
+strip.begin()
+
+load_dotenv(find_dotenv())
+server_ip = os.environ.get('SERVER_IP')
+
+# base neo pixel functions
+def signal_handler(signal, frame):
+        colorWipe(strip, Color(0,0,0))
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+def allOn(strip, color):
+    """Turn all pixels on"""
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, color)
+		strip.show()
+
+# functions called by message handlers
+def oscInput(addr, tags, stuff, source):
+  print stuff
+
+def lightBox(addr, tags, stuff, source):
+  r, b, g = stuff
+  allOn(strip, Color(b, r, g))
+
+# assign server ip and port
+server = OSCServer((server_ip, 9090))
+
+# message handlers
+server.addDefaultHandlers() #for dealing with unmatched messages
+server.addMsgHandler("/rgb", lightBox)
+
+# start thread
+server_thread= Thread(target= server.serve_forever)
+server_thread.daemon= True
+server_thread.start()
+
+# clean exit
+try:
+  while True:
+    sleep(1)
+except KeyboardInterrupt:
+  print 'done'
+  server.close()
