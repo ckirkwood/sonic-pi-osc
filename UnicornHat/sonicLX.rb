5e16b1f9528db418d3beeb2fc8248d455231af39
Callum Kirkwood <CallumKirkwood@Toms-MacBook-Air.local>
2017-07-24 23:41:57 +0100

    upload new examples

--- UnicornHat/sonicLX.rb
+++ UnicornHat/sonicLX.rb
@@ -1,14 +1,15 @@
 # SonicLX
-# sends RGB values to an OSC server
+# sends RGB values to an OSC server and syncs with a second server w/ NeoPixels
 
 use_osc "192.168.1.104", 9090
-use_bpm 100
+use_bpm 120
 
-loop do
-  sample :drum_bass_hard
-  osc "/rgb", 255, 0, 0
-  sleep 1
+live_loop :unicorn do
+  cue :tick
   sample :drum_bass_hard
   osc "/rgb", 255, 255, 255
+  sleep 1
+  sample :drum_bass_hard
+  osc "/rgb", 255, 0, 0
   sleep 1
 end
