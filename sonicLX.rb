9e013516cd0033f55a85e9da9f6f3c586fc5a729
Callum Kirkwood <CallumKirkwood@Toms-MacBook-Air.local>
2017-07-24 20:49:36 +0100

    add bass drum

--- sonicLX.rb
+++ sonicLX.rb
@@ -1,12 +1,14 @@
-# sonicLX
-# sends RGB values to an OSC server to control LEDs
+# SonicLX
+# sends RGB values to an OSC server
 
-use_osc "192.168.1.104", 22
+use_osc "192.168.1.104", 9090
 use_bpm 100
 
 loop do
-  osc "/rgb/value", 255, 0, 0
+  sample :drum_bass_hard
+  osc "/rgb", 255, 0, 0
   sleep 1
-  osc "/rgb/value", 255, 255, 255
+  sample :drum_bass_hard
+  osc "/rgb", 255, 255, 255
   sleep 1
 end
