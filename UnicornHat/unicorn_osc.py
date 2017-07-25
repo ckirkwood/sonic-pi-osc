bad079b3b03dafd57beae4a9deac76d723f90b4f
Callum Kirkwood <CallumKirkwood@Toms-MacBook-Air.local>
2017-07-25 23:35:58 +0100

    add hsv function

--- UnicornHat/unicorn_osc.py
+++ UnicornHat/unicorn_osc.py
@@ -12,11 +12,20 @@ load_dotenv(find_dotenv())
 server_ip = os.environ.get('SERVER_IP')
 
 # functions to call on receipt
-def oscInput(addr, tags, stuff, source): 
-  print stuff
+def oscInput(addr, tags, stuff, source):
+  print addr, stuff, source
 
-def all_pixels(addr, tags, stuff, source):
+def rgb(addr, tags, stuff, source):
     r, g, b = stuff
+    print(r, g, b)
+    unicorn.clear()
+    unicorn.set_all(r, g, b)
+    unicorn.show()
+
+def hsv(addr, tags, stuff, source):
+    h, s, v = stuff
+    r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(h, s, v)]
+    print(r, g, b)
     unicorn.clear()
     unicorn.set_all(r, g, b)
     unicorn.show()
@@ -25,18 +34,18 @@ def all_pixels(addr, tags, stuff, source):
 server = OSCServer((server_ip, 9090))
 
 # message handlers
-server.addDefaultHandlers() #for dealing with unmatched messages 
-server.addMsgHandler("/rgb", all_pixels) 
+server.addDefaultHandlers() #for dealing with unmatched messages
+server.addMsgHandler("/rgb", hsv)
 
 # start thread
-server_thread= Thread(target= server.serve_forever) 
-server_thread.daemon= True 
-server_thread.start() 
+server_thread= Thread(target= server.serve_forever)
+server_thread.daemon= True
+server_thread.start()
 
 # clean exit
-try: 
-  while True: 
-    sleep(1) 
-except KeyboardInterrupt: 
-  print 'done' 
+try:
+  while True:
+    sleep(1)
+except KeyboardInterrupt:
+  print 'done'
   server.close()
